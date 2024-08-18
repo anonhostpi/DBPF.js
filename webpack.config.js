48ada07f819a731c183a5a292e73c7bf3fb645bc
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-08-18 03:38:00 -0700

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
     output: {
         filename: 'dbpf.web.js',  // Output bundle file
         path: path.resolve(__dirname, 'dist'),  // Output directory
+        libraryTarget: 'umd',  // Bundle for Node.js and the browser
+        globalObject: 'globalThis',  // Fix global object in the browser
     },
     resolve: {
         extensions: ['.ts', '.js', '.json'],  // Resolve these extensions
@@ -24,6 +26,10 @@ module.exports = {
                 type: 'json',
             },
         ],
+    },
+    externals: {
+        "buffer" : "Buffer",
+        "process" : "process",
     },
     devtool: false,  // Disable source maps to match your tsconfig
     mode: 'development',  // Can be 'development' or 'production'
--- webpack.test.config.js
+++ webpack.test.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = require('./webpack.config.js');
 
 module.exports.entry = './test/test.ts';
+delete module.exports.output.libraryTarget;
+delete module.exports.output.globalObject;
 module.exports.output = {
     filename: 'test.web.js',
     path: path.resolve(__dirname, 'test')
