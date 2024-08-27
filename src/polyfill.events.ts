be00b12ac7dee22550a5e4da43c2e917096149d8
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-08-27 12:32:45 -0700

    add eventedpromise to events

--- src/polyfill.events.ts
+++ src/polyfill.events.ts
@@ -50,3 +50,48 @@ export const { EventEmitter } = polyfill(
 }
 export type EventEmitter = _EventEmitter;
 export type IEventEmitter = _EventEmitter;
+
+type EmitterOptions = {
+    emit: EventEmitMethod,
+    events: {
+        resolve: string,
+        reject: string,
+    }
+}
+
+export class EventedPromise<T> extends Promise<T> {
+    constructor(
+        executor: (
+            resolve: (value: T | PromiseLike<T>) => void,
+            reject: (reason?: any) => void
+        ) => void,
+        emit: EventEmitMethod | EmitterOptions = {
+            emit: (event: string, ...args: any[]) => false,
+            events: { resolve: "resolve", reject: "reject" }
+        }
+    ){
+        const options = typeof emit === "function" ?
+            { emit, events: { resolve: "resolve", reject: "reject" } } : emit;
+
+        function wrapped_executor (
+            engine_provided_resolve: (value: T | PromiseLike<T>) => void,
+            engine_provided_reject: (reason?: any) => void
+        ){
+            function emitting_resolve(value: T | PromiseLike<T>){
+                engine_provided_resolve(value);
+                options.emit(options.events.resolve, value);
+            };
+            function emitting_reject(reason?: any){
+                engine_provided_reject(reason);
+                options.emit(options.events.reject, reason);
+            };
+            try {
+                const wrapped_out = executor(emitting_resolve, emitting_reject);
+            } catch (e) {
+                emitting_reject(e);
+            }
+        }
+
+        super(wrapped_executor);
+    }
+}
