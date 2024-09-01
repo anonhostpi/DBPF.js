03d81aa40d358d2114d274e0b20acf9336bce7fc
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-09-01 13:08:41 -0700

    add playground to wiki

--- pub/docusaurus.config.ts
+++ pub/docusaurus.config.ts
@@ -83,6 +83,12 @@ const config: Config = {
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
+          href: 'playground',
+          target: '_blank',
+          label: 'Playground',
+          position: 'right',
+        },
+        {
           href: 'https://github.com/anonhostpi/DBPF.js',
           label: 'GitHub',
           position: 'right',
