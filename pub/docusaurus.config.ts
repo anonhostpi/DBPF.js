584cddfed244b4695075f9582d1b791f56933325
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-09-02 20:17:56 -0700

    fix playground link

--- pub/docusaurus.config.ts
+++ pub/docusaurus.config.ts
@@ -83,7 +83,7 @@ const config: Config = {
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'playground',
+          href: '/playground',
           target: '_blank',
           label: 'Playground',
           position: 'right',
