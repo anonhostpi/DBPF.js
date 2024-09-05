49a81bce3e5cb3fcb7255d87d89f41e939163274
Blue Falcon <anon@hostpi.onmicrosoft.com>
2024-09-04 22:23:56 -0700

    add more metadata to THUM

--- src/Plugins/ResourceTypes/THUM/plugin.ts
+++ src/Plugins/ResourceTypes/THUM/plugin.ts
@@ -6,6 +6,8 @@ type TaggedEntry = import("../../../DBPF").TaggedEntry;
 const data = details.tables[0];
 const ids = Object.keys(data)
 const TAG = details.tag;
+const LABEL = details.label;
+const DOC = `https://anonhostpi.github.io/DBPF.js/docs/other/spec/sections/ResourceTypes#${TAG}---${LABEL}`;
 
 export function parse( entry: IDBPFEntry, detailed?: boolean ): IDBPFEntry {
     const hex = "0x" + entry.type.toString(16).toUpperCase().padStart(8, "0");
